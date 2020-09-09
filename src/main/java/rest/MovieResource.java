8496d66c1a0da2eb83a0b7ee98e78e493c4ef06e
Lars-m <lam@cphbusiness.dk>
2020-09-09 09:19:10 +0200

    almost ready

---
+++ src/main/java/rest/MovieResource.java
@@ -0,0 +1,88 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.MovieDTO;
+
+//Make sure NOT to have any references to your Entity Classes here
+//import entities.Movie;
+
+import facades.MovieFacade;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import utils.EMF_Creator;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("movie")
+public class MovieResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+               
+    private static final MovieFacade FACADE =  MovieFacade.getMovieFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+    
+    @Path("count")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getMovieCount() {
+        long count = FACADE.getMovieCount();
+        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    }
+    
+
+    @Path("all")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAll() {
+        return GSON.toJson(FACADE.getAllMovies());
+    }
+
+    @Path("/{id}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getById(@PathParam("id") int id) {
+        return GSON.toJson(FACADE.getMovieById(id));
+    }
+
+    @Path("title/{title}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getByTitle(@PathParam("title") String title) {
+        List<MovieDTO> movies = FACADE.getMoviesByTitle(title);
+        if(movies == null || movies.isEmpty()){
+            return Response.status(404).entity("{\"code\":404,\"msg\":\"Movie not found\"}").build();
+        }
+        return Response.ok(GSON.toJson(movies)).build();
+    }
+    
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String create(MovieDTO movie) {
+        MovieDTO result = FACADE.createMovie(movie.getYear(),movie.getTitle(),movie.getActors());
+        return GSON.toJson(result);
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void update(MovieDTO entity, @PathParam("id") int id) {
+        throw new UnsupportedOperationException();
+    }
+}
